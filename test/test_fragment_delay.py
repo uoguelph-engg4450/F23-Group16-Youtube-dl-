40348385bbfc8778a0895d66d8bb48b9f4ed5596
Ignacio Ballesteros <ignacio@Ignacios-MacBook-Pro.local>
2023-12-01 19:11:09 -0500

    Added fragment retry delay logic and code.

--- test/test_fragment_delay.py
+++ test/test_fragment_delay.py
@@ -7,10 +7,11 @@ import os
 
 
 # Directory from which to serve files
-SERVE_DIR = './test/testdata/fragments'  # Update this to your serving directory path
+SERVE_DIR = "./test/testdata/fragments"  # Update this to your serving directory path
 
 # Directory to download files
-DOWNLOAD_DIR = '.'  # Update this to your download directory path
+DOWNLOAD_DIR = "."  # Update this to your download directory path
+
 
 class FlakyHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -18,32 +19,47 @@ class FlakyHTTPHandler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
         current_time = time.time()
-        #print("\n\n")
-        #print(current_time - start_time)
-        #print("\n\n")
+        # print("\n\n")
+        # print(current_time - start_time)
+        # print("\n\n")
         if random.choice([True, False, False]):
             time.sleep(random.uniform(0.5, 1))
-        if ( 5 > current_time - start_time > 3):
+        if 5 > current_time - start_time > 3:
             self.send_error(404)
             return
-        elif ( 20 > current_time - start_time > 7 ):
+        elif 20 > current_time - start_time > 7:
             self.send_error(404)
             return
         super().do_GET()
 
+
 def run_server():
-    server_address = ('127.0.0.1', 8000)
+    server_address = ("127.0.0.1", 8000)
     httpd = http.server.HTTPServer(server_address, FlakyHTTPHandler)
     httpd.serve_forever()
+
 
 server_thread = threading.Thread(target=run_server)
 server_thread.daemon = True
 server_thread.start()
 
+
 def download_with_youtube_dl():
-    url = 'http://127.0.0.1:8000/test_fragments.m3u8'
+    url = "http://127.0.0.1:8000/test_fragments.m3u8"
     try:
-        subprocess.run(['python', '-m', 'youtube_dl','--hls-prefer-native','--retry-delay', '200',  '--abort-on-unavailable-fragment', url ], check=False)
+        subprocess.run(
+            [
+                "python3",
+                "-m",
+                "youtube_dl",
+                "--hls-prefer-native",
+                "--retry-delay",
+                "200",
+                "--abort-on-unavailable-fragment",
+                url,
+            ],
+            check=False,
+        )
         print("Download completed successfully.")
     except subprocess.CalledProcessError as e:
         print("youtube-dl failed with error:", e)
